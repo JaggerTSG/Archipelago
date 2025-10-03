197743a7c49cef33da5ffba4ca6138fe67f68653
JaggerTSG <69665234+JaggerTSG@users.noreply.github.com>
2025-10-03 06:23:01 -0700

    Fusion data: items.py events
    names fixed for added events

--- worlds/metroidfusion/data/items.py
+++ worlds/metroidfusion/data/items.py
@@ -37,9 +37,9 @@ all_item_data = [
 
 events: list[tuple[str, ItemClassification, str, str]] = [
     ("Victory", ItemClassification.progression, OperationsDeck.name, "Victory"),,
-    ("Arachnus", ItemClassification.progression, Sector4PumpControl.name, "Arachnus Defeated"),
-    ("Data Courtyard Tunnel", ItemClassification.progression, Sector4PumpControl.name, "Data Courtyard Tunnel Opened"),
-    ("Nettori", ItemClassification.progression, Sector4PumpControl.name, "Nettori Defeated"),
-    ("BOX1", ItemClassification.progression, Sector4PumpControl.name, "BOX1 Defeated")
+    ("Arachnus Defeated", ItemClassification.progression, ArachnusDefeated.name, "Arachnus Defeated"),
+    ("Data Courtyard Tunnel", ItemClassification.progression, DataCourtyardTunnel.name, "Data Courtyard Tunnel Opened"),
+    ("Nettori Defeated", ItemClassification.progression, NettoriDefeated.name, "Nettori Defeated"),
+    ("BOX1 Defeated", ItemClassification.progression, BOX1Defeated.name, "BOX1 Defeated")
     ("Pump Control", ItemClassification.progression, Sector4PumpControl.name, "Pump Control Activated")
 ]
